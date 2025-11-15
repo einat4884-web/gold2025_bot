d306d9b6abdc8e6be59b22d6c80c1eab9592daff
einat4884-web <einat4884@gmail.com>
2025-11-15 16:15:01 +0200

    auto update

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: streamlit run dashboard2025.py --server.port $PORT --server.address 0.0.0.0
--- scripts/run_demo_trade.bat
+++
@@ -1,3 +0,0 @@
-@echo off  
-cd /d "C:\Users\haya1843\Downloads\Gold2025_Clean_IBKR\scripts"  
-python paper_test_trade.py 
